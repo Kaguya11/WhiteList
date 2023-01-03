ed8ca244aaf939ce3970b5b3d4a1a2e6b1049d05
Kaguya11 <106523037+Kaguya11@users.noreply.github.com>
2023-01-03 17:05:07 +0500

    Delete WhiteList.lua

--- WhiteList.lua
+++
@@ -1,7 +0,0 @@
-local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaguya11/DataBase/main/DataBase", true))()
-
-if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaguya11/Kagu-Hub-v2/main/KaguHub.lua"))()) 
-else
-  game:service('Players').LocalPlayer:Kick('Not WhiteListed')
-end
